003071c6d525804948b5335bbbb6d7770bd5f068
Giwrgos Pouliakis <georgepouliakis02@gmail.com>
2025-07-24 15:58:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// ektupwse mhnuma
+console.log("Hello");
+console.log("Welcome");
+
+// ektupwse alert
+window.alert("Error 404");
+
+document.getElementById("myh1").textContent = "Gerard"
+document.getElementById("myp").textContent = "You lose"
