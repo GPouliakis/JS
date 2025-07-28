12e5483ed7ff2e5c05c10f8b5c3b5c370c70028e
Giwrgos Pouliakis <georgepouliakis02@gmail.com>
2025-07-28 13:55:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,54 +1,33 @@
 /*
+const PI = 3.14159;
+let radius;
+let circumference;
 
-// --ektupwse mhnuma--
-console.log("Hello");
-console.log("Welcome");
-
-// --ektupwse alert--
-window.alert("Error 404");
-
-document.getElementById("myh1").textContent = "Gerard"
-document.getElementById("myp").textContent = "You lose"
-
+document.getElementById("mysubmit").onclick = function(){
+    radius = document.getElementById("mytext").value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius; 
+    console.log(circumference);
+    document.getElementById("myh3").textContent = circumference + " cm";
+}
 */
 
-/*
----Metavlites JS---
+const increaseBtn = document.getElementById("increaseBtn");
+const ResetBtn = document.getElementById("resetBtn");
+const decreaseBtn = document.getElementById("decreaseBtn");
+const countLabel = document.getElementById("countLabel");
 
-let firstName = "George";
-let age = 23;
-let price = 5.99;
-let gpa = 8.4;
-let isStudent = true;
+let count = 0;
 
-console.log(typeof price);
-
-console.log(`Your name: ${firstName} `);
-console.log(`Your age: ${age}`);
-console.log(`The price: ${price}`);
-console.log(`Your gpa: ${gpa}`);
-console.log(`Is he student?: ${isStudent}`);
-
-let gitName = "GPouliakis";
-let age = 23;
-let isStudent = true;
-
-document.getElementById("p1").textContent = `Your GitHub Name is ${gitName}`;
-document.getElementById("p2").textContent = `Your Age is ${age}`;
-document.getElementById("p3").textContent = `Are you Student? ${isStudent}`;
-
-*/
-
-let username;
-let password;
-
-document.getElementById("submit").onclick = function (){
-    username = document.getElementById("username").value;
-    password = document.getElementById("password").value;
-
-    document.getElementById("h1").textContent = `Welcome ${username}`
-
-    console.log(username);
-    console.log(password);
-    
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
+ResetBtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = count;
+}
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
 }
